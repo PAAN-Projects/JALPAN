65139e2764745ad944548e1a7650c4c9d2c39dfb
Prayag Prajapati <prayagprajapati.17@gmail.com>
2022-11-30 17:33:21 +0530

    Allow adding food items

--- jalpan/management/urls.py
+++ jalpan/management/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("addFood/", views.addFood, name="addFood"),
+    path("addFood/addFoodRecord/", views.addFoodRecord, name="addFoodRecord")
 ]
--- jalpan/management/views.py
+++ jalpan/management/views.py
@@ -1,9 +1,24 @@
+from audioop import reverse
 from re import template
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.urls import reverse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from .models import Food
 
 def index(request):
+    foodList = Food.objects.all().values()
     template = loader.get_template("main.html")
-    return HttpResponse(template.render())
-# Create your views here.
+    context = {
+        'foodList': foodList
+    }
+    return HttpResponse(template.render(context, request))
+
+def addFood(request):
+    template = loader.get_template('addFood.html')
+    return HttpResponse(template.render({}, request))
+
+def addFoodRecord(request):
+    x = request.POST['foodName']
+    food = Food(foodName=x)
+    food.save()
+    return HttpResponseRedirect(reverse('index'))
