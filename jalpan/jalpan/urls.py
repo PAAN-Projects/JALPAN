5081b5a409f5a1922d99c016fc6803f4b2e2026e
Prayag Prajapati <prayagprajapati.17@gmail.com>
2022-12-02 16:26:02 +0530

    Fix TEMPLATE_DIRS and add order app

--- jalpan/jalpan/urls.py
+++ jalpan/jalpan/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path("management/", include("management.urls")),
     path('admin/', admin.site.urls),
+    path("order/", include("order.urls"))
 ]
---
+++ jalpan/order/views.py
@@ -0,0 +1,8 @@
+from re import template
+from django.urls import reverse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+
+def index(request):
+    template = loader.get_template("order.html")
+    return HttpResponse(template.render())
