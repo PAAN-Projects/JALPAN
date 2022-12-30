8e52dabcc418c55e5b20a20a9c5dda561ecf6f93
neev7096 <neev.kansara11@gmail.com>
2022-12-30 12:22:43 +0530

    commit

--- jalpan/jalpan/urls.py
+++ jalpan/jalpan/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("management/", include("management.urls")),
-    path("icons/", include("dj_iconify.urls")),
     path('admin/', admin.site.urls),
     path("order/", include("order.urls"))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
